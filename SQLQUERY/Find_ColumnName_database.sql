ae2e50924a19b359a440673f43bacf7dc2862c79
mircoso <mircoso@mircoso.com>
2021-04-01 09:05:22 +0200

    Update Find_ColumnName_database.sql

--- SQLQUERY/Find_ColumnName_database.sql
+++ SQLQUERY/Find_ColumnName_database.sql
@@ -1,8 +1,12 @@
-SELECT      c.name  AS 'ColumnName'
-            ,t.name AS 'TableName'
+SELECT      s.name AS 'SchemaName',
+	t.name AS 'TableName',
+	c.name AS 'ColumnName'
 FROM        sys.columns c
-JOIN        sys.tables  t   ON c.object_id = t.object_id
-WHERE       c.name LIKE '%[Filed name]%'
-            and t.name like '[Table name]%'
-ORDER BY    TableName
+INNER JOIN  sys.tables  t   ON c.object_id = t.object_id
+INNER JOIN  sys.schemas s   ON t.schema_id = s.schema_id
+WHERE       c.name LIKE '%product_id%'
+            --and t.name like '%[Table name]%'
+            --and s.name like '%[Schema name]%'
+ORDER BY    SchemaName
+	,TableName
             ,ColumnName;
