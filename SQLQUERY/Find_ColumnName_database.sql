e8a6dbb9ec28377a44e17c4adf690caca97b2ff1
mircoso <mircoso@mircoso.com>
2021-04-01 09:06:30 +0200

    Update Find_ColumnName_database.sql

--- SQLQUERY/Find_ColumnName_database.sql
+++ SQLQUERY/Find_ColumnName_database.sql
@@ -1,6 +1,6 @@
 SELECT      s.name AS 'SchemaName',
-			t.name AS 'TableName',
-			c.name AS 'ColumnName'
+            t.name AS 'TableName',
+            c.name AS 'ColumnName'
 FROM        sys.columns c
 INNER JOIN  sys.tables  t   ON c.object_id = t.object_id
 INNER JOIN  sys.schemas s   ON t.schema_id = s.schema_id
@@ -8,5 +8,5 @@ WHERE       c.name LIKE '%[Column name]%'
             --and t.name like '%[Table name]%'
             --and s.name like '%[Schema name]%'
 ORDER BY    SchemaName
-			,TableName
+            ,TableName
             ,ColumnName;
